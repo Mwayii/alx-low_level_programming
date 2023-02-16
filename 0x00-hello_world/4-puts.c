3b88248d3bd6a029f5d9b88733d6a5f44084a3a0
“Mwayii” <mapundamwayi@gmail.com>
2023-02-16 13:35:31 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that uses the puts function instead of the printf
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
