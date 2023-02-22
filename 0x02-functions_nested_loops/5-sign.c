83d86d43f6889bfe5ae8099d033db596e554b310
“Mwayii” <mapundamwayi@gmail.com>
2023-02-22 13:52:52 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,24 +5,25 @@
  *
  * @n: number to check
  *
- * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than zero
+ * Return: 1 if n is greater than zero, 0 if n is zero,
+ *  -1 if n is less than zero
  */
 int print_sign(int n)
 {
-        if (n > 0)
-        {
-                _putchar('+');
-                return (1);
-        }
-        else if (n == 0)
-        {
-                _putchar('0');
-                return (0);
-        }
-        else
-        {
-                _putchar('-');
-                return (-1);
-        }
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-')
+		return (-1)
+	}
 }
 
